b1f4d17bc73ad72f08e2ebbe527ac6c2ce466c0a
Sami Salonen <ssalonen@gmail.com>
2017-06-06 19:08:08 +0300

    write api more similar to read api

--- addons/io/org.openhab.io.transport.modbus/src/main/java/org/openhab/io/transport/modbus/ModbusManager.java
+++ addons/io/org.openhab.io.transport.modbus/src/main/java/org/openhab/io/transport/modbus/ModbusManager.java
@@ -1,36 +1,65 @@
 package org.openhab.io.transport.modbus;
 
+import java.util.concurrent.ScheduledFuture;
+
 import org.openhab.io.transport.modbus.endpoint.EndpointPoolConfiguration;
 import org.openhab.io.transport.modbus.endpoint.ModbusSlaveEndpoint;
 
 public interface ModbusManager {
 
     public interface PollTask {
-        // metadata, e.g. last polled?
+        /**
+         * Gets endpoint associated with this poll task
+         *
+         * @return
+         */
         ModbusSlaveEndpoint getEndpoint();
 
+        /**
+         * Gets request associated with this poll task
+         *
+         * @return
+         */
         ModbusReadRequestBlueprint getRequest();
 
+        /**
+         * Gets callback that will be called with the response
+         *
+         * @return
+         */
         ModbusReadCallback getCallback();
     }
 
-    public void executeOneTimePoll(PollTask task);
+    public interface WriteTask {
+        /**
+         * Gets endpoint associated with this write task
+         *
+         * @return
+         */
+        ModbusSlaveEndpoint getEndpoint();
 
-    /**
-     *
-     * @param endpoint
-     * @param message
-     * @param config
-     * @param pollPeriodMillis
-     * @param initialDelayMillis
-     * @return string identifier for the poll
-     */
-    void registerRegularPoll(PollTask task, long pollPeriodMillis, long initialDelayMillis);
+        /**
+         * Gets request associated with this write task
+         *
+         * @return
+         */
+        ModbusWriteRequestBlueprint getRequest();
+
+        /**
+         * Gets callback that will be called with the response
+         *
+         * @return
+         */
+        ModbusWriteCallback getCallback();
+    }
+
+    public ScheduledFuture<?> submitOneTimePoll(PollTask task);
+
+    public void registerRegularPoll(PollTask task, long pollPeriodMillis, long initialDelayMillis);
 
     public boolean unregisterRegularPoll(PollTask task);
 
-    public void writeCommand(ModbusSlaveEndpoint endpoint, ModbusWriteRequestBlueprint request,
-            ModbusWriteCallback callback);
+    public ScheduledFuture<?> submitOneTimeWrite(WriteTask task);
 
     /**
      * Configure general connection settings with a given endpoint
