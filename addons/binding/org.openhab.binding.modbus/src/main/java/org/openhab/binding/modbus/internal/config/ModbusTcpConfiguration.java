6c773839806fd97204a8369792119f3ca7bbc1db
Sami Salonen <ssalonen@gmail.com>
2017-06-17 13:21:49 +0300

    more tcp params, serial endpoint xml drafting

--- addons/binding/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/config/ModbusTcpConfiguration.java
+++ addons/binding/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/config/ModbusTcpConfiguration.java
@@ -4,6 +4,11 @@ public class ModbusTcpConfiguration {
     private String host;
     private int port;
     private int id;
+    private int timeBetweenTransactionsMillis;
+    private int timeBetweenReconnectMillis;
+    private int connectMaxTries;
+    private int reconnectAfterMillis;
+    private int connectTimeoutMillis;
 
     public String getHost() {
         return host;
@@ -27,6 +32,46 @@ public class ModbusTcpConfiguration {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getTimeBetweenTransactionsMillis() {
+        return timeBetweenTransactionsMillis;
+    }
+
+    public void setTimeBetweenTransactionsMillis(int timeBetweenTransactionsMillis) {
+        this.timeBetweenTransactionsMillis = timeBetweenTransactionsMillis;
+    }
+
+    public int getTimeBetweenReconnectMillis() {
+        return timeBetweenReconnectMillis;
+    }
+
+    public void setTimeBetweenReconnectMillis(int timeBetweenReconnectMillis) {
+        this.timeBetweenReconnectMillis = timeBetweenReconnectMillis;
+    }
+
+    public int getConnectMaxTries() {
+        return connectMaxTries;
+    }
+
+    public void setConnectMaxTries(int connectMaxTries) {
+        this.connectMaxTries = connectMaxTries;
+    }
+
+    public int getReconnectAfterMillis() {
+        return reconnectAfterMillis;
+    }
+
+    public void setReconnectAfterMillis(int reconnectAfterMillis) {
+        this.reconnectAfterMillis = reconnectAfterMillis;
+    }
+
+    public int getConnectTimeoutMillis() {
+        return connectTimeoutMillis;
+    }
+
+    public void setConnectTimeoutMillis(int connectTimeoutMillis) {
+        this.connectTimeoutMillis = connectTimeoutMillis;
     }
 
 }
